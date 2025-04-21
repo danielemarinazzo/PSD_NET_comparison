c26cd9937c19274da17ef4ad8ecf3b511a6f2fda
dmarinaz <dmarinaz@192.168.129.72>
2025-04-21 15:57:52 +0200

    faster

--- AEC_noorth.m
+++ AEC_noorth.m
@@ -1,22 +1,21 @@
-function AEC=AEC_noorth(a)
+function AEC = AEC_noorth(a)
 % a is a filtered multichannel signal (time x channels)
 
-N=size(a,2);
-AEC(1:N,1:N)=0;
-complex_a=hilbert(a);
+N = size(a, 2);
+AEC = zeros(N, N);
 
-for i=1:N
-    for j=1:N
-        if i<j        
-
-        AEC1=abs(corrcoef(abs(complex_a(:,j)),abs(complex_a(:,i))));        
-        AEC_mean=AEC1(1,2);        
-        AEC(i,j)=AEC_mean;       
-        AEC(j,i)=AEC(i,j);
-        
-        end
-        
-    end
+% Compute amplitude envelope using analytic method
+amp = zeros(size(a));
+for ch = 1:N
+    amp(:, ch) = envelope(a(:, ch), 1, 'analytic');
 end
 
+% Compute pairwise AEC
+for i = 1:N
+    for j = i+1:N
+        AEC1 = abs(corrcoef(amp(:,i), amp(:,j)));
+        AEC(i,j) = AEC1(1,2);
+        AEC(j,i) = AEC(i,j);  % symmetry
+    end
+end
 end
